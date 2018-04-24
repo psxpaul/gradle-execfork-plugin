c655dd5774cf203dfcf4eb3c1ee32ca27dee70ec
Paul Roberts <proberts@5cg7164p9q.st.com>
2018-04-23 17:11:02 -0700

    Bump kotlin and gradle versions, and re-write build scripts in kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+  id("com.gradle.plugin-publish").version("0.9.7")
+  id("org.jetbrains.kotlin.jvm").version("1.2.40")
+  id("idea")
+  id("maven")
+}
+
+group = "com.github.psxpaul"
+version = File("VERSION").readText().trim()
+buildDir = File("build/gradle")
+
+dependencies {
+  compile(gradleApi())
+  compile("org.jetbrains.kotlin:kotlin-stdlib:1.2.40")
+
+  testCompile("junit:junit:4.12")
+  testCompile("org.hamcrest:hamcrest-all:1.3")
+}
+
+pluginBundle {
+  website = "http://github.com/psxpaul"
+  vcsUrl = "https://github.com/psxpaul/gradle-execfork-plugin"
+  description = "Execute Java or shell processes in the background during a build"
+  tags = listOf("java", "exec", "background", "process")
+
+  (plugins) {
+    "greetingsPlugin" {
+      id = "com.github.psxpaul.execfork"
+      displayName = "Gradle Exec Fork Plugin"
+    }
+  }
+}
+
+repositories {
+  mavenLocal()
+  mavenCentral()
+}
+
+tasks {
+    val cleanSampleProjects by creating(GradleBuild::class) {
+      buildFile = File("${project.rootDir}/sample_projects/build.gradle")
+      tasks = listOf("clean")
+    }
+    cleanSampleProjects.dependsOn("install")
+    "clean" { finalizedBy(cleanSampleProjects) }
+
+    val buildSampleProjects by creating(GradleBuild::class) {
+      buildFile = File("${project.rootDir}/sample_projects/build.gradle")
+      tasks = listOf("build")
+    }
+    buildSampleProjects.dependsOn("install")
+    "build" { finalizedBy(buildSampleProjects) }
+}
+
+
+val javadocJar by tasks.creating(Jar::class) {
+  classifier = "javadoc"
+  from("javadoc")
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+  classifier = "sources"
+  from(java.sourceSets["main"].allSource)
+}
+
+artifacts {
+  add("archives", javadocJar)
+  add("archives", sourcesJar)
+}
+
