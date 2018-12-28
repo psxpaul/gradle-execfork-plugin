fa1ec53ae256c22f942f867eef2d5f212226ce7c
Florian Dreier <dreier@cqse.eu>
2018-12-28 11:53:02 +0100

    Formatted build.gradle.kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  id("com.gradle.plugin-publish").version("0.9.7")
-  id("org.jetbrains.kotlin.jvm").version("1.2.40")
-  id("idea")
-  id("maven")
+    id("com.gradle.plugin-publish").version("0.9.7")
+    id("org.jetbrains.kotlin.jvm").version("1.2.40")
+    id("idea")
+    id("maven")
 }
 
 group = "com.github.psxpaul"
@@ -10,43 +10,43 @@ version = File("VERSION").readText().trim()
 buildDir = File("build/gradle")
 
 dependencies {
-  compile(gradleApi())
-  compile("org.jetbrains.kotlin:kotlin-stdlib:1.2.40")
+    compile(gradleApi())
+    compile("org.jetbrains.kotlin:kotlin-stdlib:1.2.40")
 
-  testCompile("junit:junit:4.12")
-  testCompile("org.hamcrest:hamcrest-all:1.3")
+    testCompile("junit:junit:4.12")
+    testCompile("org.hamcrest:hamcrest-all:1.3")
 }
 
 pluginBundle {
-  website = "http://github.com/psxpaul"
-  vcsUrl = "https://github.com/psxpaul/gradle-execfork-plugin"
-  description = "Execute Java or shell processes in the background during a build"
-  tags = listOf("java", "exec", "background", "process")
+    website = "http://github.com/psxpaul"
+    vcsUrl = "https://github.com/psxpaul/gradle-execfork-plugin"
+    description = "Execute Java or shell processes in the background during a build"
+    tags = listOf("java", "exec", "background", "process")
 
-  (plugins) {
-    create("execForkPlugin") {
-      id = "com.github.psxpaul.execfork"
-      displayName = "Gradle Exec Fork Plugin"
+    (plugins) {
+        create("execForkPlugin") {
+            id = "com.github.psxpaul.execfork"
+            displayName = "Gradle Exec Fork Plugin"
+        }
     }
-  }
 }
 
 repositories {
-  mavenLocal()
-  mavenCentral()
+    mavenLocal()
+    mavenCentral()
 }
 
 tasks {
     val cleanSampleProjects by creating(GradleBuild::class) {
-      buildFile = File("${project.rootDir}/sample_projects/build.gradle")
-      tasks = listOf("clean")
+        buildFile = File("${project.rootDir}/sample_projects/build.gradle")
+        tasks = listOf("clean")
     }
     cleanSampleProjects.dependsOn("install")
     "clean" { finalizedBy(cleanSampleProjects) }
 
     val buildSampleProjects by creating(GradleBuild::class) {
-      buildFile = File("${project.rootDir}/sample_projects/build.gradle")
-      tasks = listOf("build")
+        buildFile = File("${project.rootDir}/sample_projects/build.gradle")
+        tasks = listOf("build")
     }
     buildSampleProjects.dependsOn("install")
     "build" { finalizedBy(buildSampleProjects) }
@@ -54,17 +54,17 @@ tasks {
 
 
 val javadocJar by tasks.creating(Jar::class) {
-  classifier = "javadoc"
-  from("javadoc")
+    classifier = "javadoc"
+    from("javadoc")
 }
 
 val sourcesJar by tasks.creating(Jar::class) {
-  classifier = "sources"
-  from(sourceSets["main"].allSource)
+    classifier = "sources"
+    from(sourceSets["main"].allSource)
 }
 
 artifacts {
-  add("archives", javadocJar)
-  add("archives", sourcesJar)
+    add("archives", javadocJar)
+    add("archives", sourcesJar)
 }
 
